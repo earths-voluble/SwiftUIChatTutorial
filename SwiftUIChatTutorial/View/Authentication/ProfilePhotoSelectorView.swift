86bc47537b9752cdb70d17c4aa306da31ff501c4
Paul Lee <earths-voluble.0g@icloud.com>
2023-03-08 21:05:15 +0900

    230308

---
+++ SwiftUIChatTutorial/View/Authentication/ProfilePhotoSelectorView.swift
@@ -0,0 +1,20 @@
+//
+//  ProfilePhotoSelectorView.swift
+//  SwiftUIChatTutorial
+//
+//  Created by 이보한 on 2023/03/07.
+//
+
+import SwiftUI
+
+struct ProfilePhotoSelectorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProfilePhotoSelectorView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfilePhotoSelectorView()
+    }
+}
